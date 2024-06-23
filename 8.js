1ddfbbf61e577ad165bee66ae18f2518335bd162
Power898 <limbo22city@gmail.com>
2024-06-23 23:13:05 +0300

    my homework

--- 8.js
+++ 8.js
@@ -1,13 +1,16 @@
 console.log("Завдання: 8 ==============================");
 
-function task8() {
+function task8() {function fetchFakeData() { const fakeData = { name: "John",age: 30}; return Promise.resolve(fakeData);} 
+fetchFakeData()
+ .then((resolve) => console.log(resolve))
+ .catch((error) => console.log(error));
+}
   // Створюємо функцію fetchFakeData, яка симулює запит до сервера та повертає "дані" через Promise.resolve().
   // Створюємо fakeData константу з данними { name: "John",age: 30}
   // Використовуємо Promise.resolve(fakeData) для того, щоб створити вже зарезолвлений проміс.
   // Викликаємо функцію fetchFakeData
   // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
   // Якщо проміс виконаний з помилкою виводимо в консоль помилку
-}
 
 // Викликаємо функцію task8
 task8();
--- 9.js
+++ 9.js
@@ -1,13 +1,17 @@
 console.log("Завдання: 9 ==============================");
 
-function task9() {
+function task9() {function fetchWithError() {const errorMessage = "Помилка при з'єднанні з сервером";  return Promise.reject(new Error(errorMessage));  }
   // Створюємо функцію fetchWithError, яка симулює помилку при запиті до сервера.
   // Створюємо константу errorMessage в яку записуємо рядок "Помилка при з'єднанні з сервером"
   // Використовуємо Promise.reject(errorMessage) для створення вже відхиленого промісу.
+  fetchWithError()
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+}
+
   // Викликаємо функцію fetchWithError
   // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
   // Якщо проміс виконаний з помилкою виводимо в консоль помилку
-}
 
 // Викликаємо функцію task10
 task9();
